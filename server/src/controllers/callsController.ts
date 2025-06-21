83b9a7dbd4c116dc31e9f53ba609d3e785fd0c9a
TomBer101 <tomerm1233@gmail.com>
2025-06-21 17:42:53 +0300

    Update callsController.ts
    * Add tag
    * Get calls
    * Add (specific) task

--- server/src/controllers/callsController.ts
+++ server/src/controllers/callsController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
-import { createCall, getCalls } from '../services/callsService';
+import { addTagToCall, addTaskToCall, createCall, getCalls } from '../services/callsService';
 import { CustomError } from '../classes/CustomError';
+import { ICall } from '@/models/Call';
 
 interface CreateCallRequest {
-
     name: string;
+}
+
+interface AddTagToCallRequest {
+    tagIds: string[];
+}
+
+interface AddTaskToCallRequest {
+    name?: string;
+    suggestedTaskId?: string;
+}
+
+interface UpdateCallParams {
+    callId: string;
 }
 
 export const createCallController = async (req: Request<{}, {}, CreateCallRequest>, res: Response): Promise<void> => {
@@ -22,4 +35,56 @@ export const createCallController = async (req: Request<{}, {}, CreateCallReques
     }
 }
 
+export const addTagToCallController = async (req: Request<UpdateCallParams, {}, AddTagToCallRequest>, res: Response): Promise<void> => {
+    const { callId } = req.params;
+    const { tagIds } = req.body;
+
+    try {
+        const call = await addTagToCall(callId, tagIds);
+        res.status(200).json(call);
+    } catch (error) {
+        if (error instanceof CustomError) {
+            res.status(error.code).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+}
+
+export const addTaskToCallController = async (req: Request<UpdateCallParams, {}, AddTaskToCallRequest>, res: Response): Promise<void> => {
+    const { callId } = req.params;
+    const { name, suggestedTaskId } = req.body;
+
+    try {
+        let result: ICall;
+        if (suggestedTaskId) {
+            result = await addTaskToCall(callId, suggestedTaskId);
+        } else if (name) {
+            result = await addTaskToCall(callId, name);
+        } else {
+            throw new CustomError('No task name or suggested task ID provided', 400);
+        }
+
+        res.status(200).json(result);
+    } catch (error: any) {
+        if (error.name === 'CustomError') {
+            res.status(error.code).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+}
+
+export const getCallsController = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const calls = await getCalls();
+        res.status(200).json(calls);
+    } catch (error: any) {
+        if (error.name === 'CustomError') { 
+            res.status(error.code).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+}
 
