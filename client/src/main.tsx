74f5408239cb7b4bead540bc9f8076424d401ce5
TomBer101 <tomerm1233@gmail.com>
2025-06-22 21:21:52 +0300

    Update main.tsx
    Using store instead of react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { Provider } from 'react-redux'
+import store from './app/store.ts'
 
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </Provider>
   </StrictMode>,
 )
