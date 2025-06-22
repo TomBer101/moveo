b6445c509b71b6586bfd64f7d3ba2a3c5d8bcfb9
TomBer101 <tomerm1233@gmail.com>
2025-06-22 11:46:01 +0300

    Create api.ts
    A generic module to interact with the server api

---
+++ client/src/lib/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const apiClient = {
+    get: <T>(url: string) => api.get<T>(url).then(res => res.data),
+    post: <T, D = unknown>(url: string, data: D) => api.post<T>(url, data).then(res => res.data),
+}
+
+export default api;
+
