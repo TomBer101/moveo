d2272675fbcff05e022b269e0f2605e474994b5a
TomBer101 <tomerm1233@gmail.com>
2025-06-22 12:28:15 +0300

    Create reactQuery.tsx
    Configs

---
+++ client/src/configs/reactQuery.tsx
@@ -0,0 +1,24 @@
+import { type ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            staleTime: 5 * 60 * 1000, // 5 minutes
+        },
+    },
+});
+
+interface ReactQueryProviderProps {
+    children: ReactNode;
+}
+
+export const ReactQueryProvider = ({ children }: ReactQueryProviderProps) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
