ae383ed3a972f6af11be7ae023ea9449d20c57d7
TomBer101 <tomerm1233@gmail.com>
2025-06-23 22:26:45 +0300

    Update server.ts
    client url env var

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
