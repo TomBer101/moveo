4b5bfb4a1be540a9178c6f86343b0ac789071800
TomBer101 <tomerm1233@gmail.com>
2025-06-24 01:12:57 +0300

    Update config.ts

--- server/src/lib/sessions/config.ts
+++ server/src/lib/sessions/config.ts
@@ -20,7 +20,7 @@ export const sessionConfig: session.SessionOptions = {
   }),
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000, 
     sameSite: 'none'
   },
