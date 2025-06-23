a27d3a1b1688cca1d9a554c3ba98fedfbd2d0744
TomBer101 <tomerm1233@gmail.com>
2025-06-23 21:48:12 +0300

    Delete useTag.ts
    no more react query usgae

--- client/src/hooks/useTag.ts
+++
@@ -1,8 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query";
-import type { ITag } from "../types";
-
-export function useTag(tagId: string): ITag | undefined  {
-    const queryClient = useQueryClient();
-    const tags = queryClient.getQueryData<ITag[]>(['tags']);
-    return tags?.find((tag) => tag._id === tagId);
-}
