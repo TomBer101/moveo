b622048b871e838fc1117c68f41e788b841dc727
TomBer101 <tomerm1233@gmail.com>
2025-06-24 00:37:33 +0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,6 +23,7 @@ const corsOptions = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Set-Cookie']
 };
 
 app.use(cors(corsOptions));
